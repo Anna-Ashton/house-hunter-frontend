260d5eebb64594066db049a258b84d4d039608fa
Chege Ian <chegeian65@gmail.com>
2022-12-16 23:01:48 +0300

    homepage edit

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,9 +5,13 @@ const Navbar = () => {
     return (
       <>
         <Nav>
-          <Bars />
+        <a href="#" class="navbar-brand text-uppercase font-italic"><img src="https://images.unsplash.com/photo-1619216083420-6e54b895f730?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGhvdXNlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="logo"
+                class=" round rounded-circle"></img></a>
     
+            <Bars />
+            
           <NavMenu>
+          
             <NavLink to='/home' activeStyle>
                Home
             </NavLink>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,47 @@
 import React from 'react';
   
 const Home = () => {
+
+  const text=" RELAX.... WE HUNTED A HOME FOR YOU! "; 
+
+let index=0;
+
+function autowrite(){
+    document.querySelector('.underText').innerHTML=text.slice(0,index);
+    ++index;
+
+    if(index>text.length){
+        index=0;
+    }
+}
+setInterval(autowrite,100);
   return (
-    <div
+    
+    <>
+     <div
       style={{
-        display: 'flex',
+        display: '',
         justifyContent: 'center',
         alignItems: 'center',
-        height: '10vh'
+        
       }}
-    >
-      <h1>Homepage</h1>
-    </div>
+    ></div>
+    <header id="header" class="py-5 bg-light">
+      <div class="container">
+          <div class="row height align-items-center ">
+              <div class="col height-80  text-center">
+                  <h1 class="text-white display- font-weight-bold text-uppercase font-italic">
+                      <strong>WE DONT FIND HOUSES ,WE FIND HOMES !</strong>
+  
+                  </h1>
+                  <p class="underText text-white display-">Life is short, buy a house</p>
+                  </div>
+            </div>
+            
+              </div>
+        </header>
+    </>
+      
     
   );
 };
