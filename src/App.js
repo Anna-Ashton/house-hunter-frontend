79eb58015d0b52edaf6403e4760db2cf15ba683c
Chege Ian <chegeian65@gmail.com>
2022-12-16 11:12:44 +0300

    edited the src folder and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React from 'react';
 import './App.css';
-import Navbar from './navbar/Navbar';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+import Houses from './pages/houses';
+import Contact from './pages/contact';
+import SignUp from './pages/signup';
+  
 function App() {
   return (
-    <div >
-      <Navbar/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/home' exact component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/houses' component={Houses} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/sign-up' component={SignUp} />
+      </Routes>
+    </Router>
   );
 }
-
+  
 export default App;
