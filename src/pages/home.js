f6d4d97c68087639187ef45baebe08321dd85c37
Chege Ian <chegeian65@gmail.com>
2023-01-09 10:43:22 +0300

    about us

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { CDBFooter } from 'cdbreact';
   
 const Home = () => {
 
@@ -24,7 +25,8 @@ setInterval(autowrite,100);
         justifyContent: 'center',
         alignItems: 'center',
         
-      }}
+      }
+    }
     ></div>
     <header id="header" class="py-5 bg-light">
       <div class="container">
@@ -40,6 +42,72 @@ setInterval(autowrite,100);
             
               </div>
         </header>
+
+        <tr></tr>
+  <footer>
+    <div className="row p-5 d-flex justify-content-center">
+      <div className="col-lg-2">
+
+     
+        <p>Need a home ? <tr></tr>Leave the hustle to us</p>
+        <img src="https://ld-wp73.template-help.com/woocommerce/prod_24608/v2/wp-content/uploads/2019/06/fb.svg" alt=""></img>
+        <img src="https://ld-wp73.template-help.com/woocommerce/prod_24608/v2/wp-content/uploads/2019/06/instagram.svg"
+          alt=""></img>
+        <img src="https://ld-wp73.template-help.com/woocommerce/prod_24608/v2/wp-content/uploads/2019/06/youtube.svg"
+          alt=""></img>
+        <img src="https://ld-wp73.template-help.com/woocommerce/prod_24608/v2/wp-content/uploads/2019/06/twitter.svg"
+          alt=""></img>
+      </div>
+
+
+
+      
+
+      <div className="col-lg-2">
+        <p className="quicklinks">Quick Links</p>
+        <p>– About</p>
+        <p>– photos</p>
+        <p>– News</p>
+        <p>– Contacts</p>
+
+      </div>
+
+      <div className="col-lg-2">
+        <p className="quicklinks">Help</p>
+        <p>– FAQs</p>
+        <p>– Updates</p>
+        <p>– Complains</p>
+        <p>– Contacts</p>
+
+      </div>
+
+      <div className="col-lg-2">
+        <p className="quicklinks">Categories</p><tr></tr>
+        <p>– FAQs</p>
+        <p>– Rentals</p>
+        <p>– Apartments</p>
+        <p>– Contacts</p>
+
+      </div>
+
+      <div className="col-lg-2">
+        <p className="quicklinks">Contacts</p>
+        <p className="number">
+          <ion-icon name="call"></ion-icon>254-100-161
+        </p>
+        <p>
+          <ion-icon name="pin"></ion-icon>254 Souths Suited 21,<tr></tr>
+           NRB
+        </p>
+        <p className="number2">
+          <ion-icon name="mail"></ion-icon>Eaglehouse@info.co.ke
+        </p>
+
+
+      </div>
+       <h6>2022 @ Eaglehousehunter.com </h6>
+    </div>
+    </footer>   
     </>
       
     
