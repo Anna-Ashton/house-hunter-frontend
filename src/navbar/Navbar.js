bef701945cd2e27dba1d71815997e759efa75fa0
Doreen <doreenlulu97@gmail.com>
2023-01-09 10:20:10 +0300

    Created housing component

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from '../homepage/Home'
 import Login from '../login/Login'
 import SignUp from '../signup/SignUp'
+import Housing from '../housing/Housing'
 import "./navbar.css"
 
 export default function Navbar() {
@@ -10,6 +11,7 @@ export default function Navbar() {
         <div><Home/></div>
         <div><Login/></div>
         <div><SignUp/></div>
+        <div><Housing/></div>
     </div>
     
   )
