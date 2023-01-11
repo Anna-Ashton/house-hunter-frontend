5c3dd0975253322dc8e3051d1888cb5485926a49
Doreen <doreenlulu97@gmail.com>
2023-01-11 22:52:22 +0300

    Updates on House component

--- src/pages/houses.js
+++ src/pages/houses.js
@@ -1,46 +1,56 @@
-import React from 'react'
-import Houses from './houses'
+import React, {onSearchClick} from 'react'
+import { MDBBtn } from 'mdb-react-ui-kit'
+import { CDBFooter } from 'cdbreact';
+import SearchField from 'react-search-field';
+
 
 function houses() {
 
   return (
-    <div className="houses">
+    <div className="houses__container">
       <h1>Apartments Available</h1>
-      <div className="houses__container">
-        <div className="houses__wrapper">
-
+      <SearchField 
+        placeholder='Search House'
+        onChange={onSearchClick}
+        searchText="Search for Houses"
+      />
+      <div className="houses__wrapper">
           <ul className="houses__list">
-            <img src = "https://www.mashvisor.com/blog/wp-content/uploads/2018/04/bigstock-Row-Of-New-Suburban-Homes-55511546.jpg" />
-            <h5>4 Bedroom Apartment in Jamhuri Estate, Ngong Road</h5>
+            <img src = "https://commercialpropertykenya.com/wp-content/uploads/2020/01/Mahiga-Homes-11.jpeg" />
+            <h4>4 Bedroom Apartment in Jamhuri Estate, Ngong Road</h4>
+            <MDBBtn size = 'small' >KSH 60,000 </MDBBtn>
           </ul>
 
           <ul className="houses__list">
             <img src = "https://commercialpropertykenya.com/wp-content/uploads/2020/01/Mahiga-Homes-11.jpeg" />
             <h5>3 bedroom apartment in Athi View Estate, Syokimau</h5>
+            <MDBBtn size = 'small' >KSH 50,000 </MDBBtn>
           </ul>
 
           <ul className="houses__list">
-            <img src = "https://victormatara.com/wp-content/uploads/2017/11/10-Best-Real-Estate-Websites-in-Kenya-for-Property-Search-e1512050996421.jpg" />
+            <img src = "https://commercialpropertykenya.com/wp-content/uploads/2020/01/Mahiga-Homes-11.jpeg" />
             <h5>3 bedroom apartment in Airways Estate, Utawala</h5>
+            <MDBBtn size = 'small' >KSH 45,000 </MDBBtn>
           </ul>
 
           <ul className="houses__list">
-            <img src = "https://www.mashvisor.com/blog/wp-content/uploads/2018/04/bigstock-Row-Of-New-Suburban-Homes-55511546.jpg" />
+            <img src = "https://commercialpropertykenya.com/wp-content/uploads/2020/01/Mahiga-Homes-11.jpeg" />
             <h5>4 bedroom maisonette in Mirema Greens Estate, Off Mirema Drive</h5>
+            <MDBBtn size = 'small' >KSH 40,000 </MDBBtn>
           </ul>
 
           <ul className="houses__list">
-            <img src = "https://www.mashvisor.com/blog/wp-content/uploads/2018/04/bigstock-Row-Of-New-Suburban-Homes-55511546.jpg" />
+            <img src = "https://commercialpropertykenya.com/wp-content/uploads/2020/01/Mahiga-Homes-11.jpeg" />
             <h5>4 bedroom apartments in Mirema Greens Estate, Off Mirema Drive</h5>
+            <MDBBtn size = 'small' >KSH 8,000,000 </MDBBtn>
           </ul>
 
           <ul className="houses__list">
-            <img src = "https://www.mashvisor.com/blog/wp-content/uploads/2018/04/bigstock-Row-Of-New-Suburban-Homes-55511546.jpg" />
+            <img src = "https://commercialpropertykenya.com/wp-content/uploads/2020/01/Mahiga-Homes-11.jpeg" />
             <h5>5 bedroom apartments in Mirema Greens Estate, Off Mirema Drive</h5>
+            <MDBBtn size = 'small' >KSH 80,000 </MDBBtn>
           </ul>
-
-    </div>
-    </div>
+      </div>
     </div>
   );
 };
