721fbcf084c222c29c9abf1bc3a4606216b6fd4b
Doreen <doreenlulu97@gmail.com>
2023-01-11 21:02:53 +0300

    Start on house component

--- src/pages/houses.js
+++ src/pages/houses.js
@@ -1,18 +1,48 @@
-import React from 'react';
-  
-const Houses = () => {
+import React from 'react'
+import Houses from './houses'
+
+function houses() {
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '10vh'
-      }}
-    >
-      <h1>Homes</h1>
-   </div>
+    <div className="houses">
+      <h1>Apartments Available</h1>
+      <div className="houses__container">
+        <div className="houses__wrapper">
+
+          <ul className="houses__list">
+            <img src = "https://www.mashvisor.com/blog/wp-content/uploads/2018/04/bigstock-Row-Of-New-Suburban-Homes-55511546.jpg" />
+            <h5>4 Bedroom Apartment in Jamhuri Estate, Ngong Road</h5>
+          </ul>
+
+          <ul className="houses__list">
+            <img src = "https://commercialpropertykenya.com/wp-content/uploads/2020/01/Mahiga-Homes-11.jpeg" />
+            <h5>3 bedroom apartment in Athi View Estate, Syokimau</h5>
+          </ul>
+
+          <ul className="houses__list">
+            <img src = "https://victormatara.com/wp-content/uploads/2017/11/10-Best-Real-Estate-Websites-in-Kenya-for-Property-Search-e1512050996421.jpg" />
+            <h5>3 bedroom apartment in Airways Estate, Utawala</h5>
+          </ul>
+
+          <ul className="houses__list">
+            <img src = "https://www.mashvisor.com/blog/wp-content/uploads/2018/04/bigstock-Row-Of-New-Suburban-Homes-55511546.jpg" />
+            <h5>4 bedroom maisonette in Mirema Greens Estate, Off Mirema Drive</h5>
+          </ul>
+
+          <ul className="houses__list">
+            <img src = "https://www.mashvisor.com/blog/wp-content/uploads/2018/04/bigstock-Row-Of-New-Suburban-Homes-55511546.jpg" />
+            <h5>4 bedroom apartments in Mirema Greens Estate, Off Mirema Drive</h5>
+          </ul>
+
+          <ul className="houses__list">
+            <img src = "https://www.mashvisor.com/blog/wp-content/uploads/2018/04/bigstock-Row-Of-New-Suburban-Homes-55511546.jpg" />
+            <h5>5 bedroom apartments in Mirema Greens Estate, Off Mirema Drive</h5>
+          </ul>
+
+    </div>
+    </div>
+    </div>
   );
 };
   
-export default Houses;
+export default houses;
