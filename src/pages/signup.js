2dd1e0c075e279fe1c0bc5ae8fcd9dc5e5e87e9c
J.MIKE <j.mike@pop-os.localdomain>
2023-01-16 09:18:59 +0300

    my signup changes

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,41 +1,201 @@
 import React, { useState } from 'react';
 
 const SignUp = () => {
-  const [userInfo, setUserInfo] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
-  });
 
-  const handleChange = event => {
-    event.persist();
-    setUserInfo(userInfo => ({
-      ...userInfo,
-      [event.target.name]: event.target.value
-    }));
-  }
+  
+  // const [userInfo, setUserInfo] = useState({
+  //   username: '',
+  //   email: '',
+  //   password_digest: '',
+  //   // confirmPassword: ''
+  // });
+    const [email,setEmail] = useState("")
+const [password, setPassword] = useState("")
+const [fullName, setFullName] = useState("")
+const [ userName, setUserName] =useState ("")
+
+const url = "http://127.0.0.1:3000/sign"
+
+
+
+  // const handleChange = event => {
+  //   event.persist();
+  //   setUserInfo(userInfo => ({
+  //     ...userInfo,
+  //     [event.target.name]: event.target.value
+  //   }));
+  // }
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(userInfo);
+
+      fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    email,
+    password,
+    full_name:fullName ,
+    username: userName,
+  }),
+})
+  .then((r) => r.json())
+  .then((data) => {
+    // save the token to localStorage for future access
+    localStorage.setItem("jwt", data.jwt);
+    // save the user somewhere (in state!) to log the user in
+    // setUser(data.user);
+
+  });
+    console.log(email,password,fullName);
     // Send a request to the server with the user's information
     // If the information is valid, create a new account
     // If the information is invalid, display an error message
   }
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '10vh'
-      }}
-    >
-      <h1>Sign Up</h1>
+    <div className="vh-100">
+    <div className="container py-5 h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div className="card shadow-2-strong">
+            <div className="card-body p-5 text-center"></div>
+
+    <form onSubmit={handleSubmit}>
+    <h3 className="container mb-5" id="sign-in">SIGN UP</h3>
+              <p className="mb-4">Please fill in the details!</p>
+              <div className="form-outline mb-4">   
+      <label>
+        fullName:
+        <input
+          type="text"
+          name="fullname"
+          className="form-control form-control-lg"
+              value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+        />
+      </label>
+            <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          className="form-control form-control-lg"
+              value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </label>
+      </div>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          className="form-control form-control-lg"
+              value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          className="form-control form-control-lg"
+              value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      {/* <label>
+        Confirm Password:
+        <input
+          type="password"
+          name="confirmPassword"
+          className="form-control form-control-lg"
+          value={userInfo.confirmPassword}
+          onChange={handleChange}
+        />
+      </label> */}
+    </form>
+    <div class="text-center p-4">
+          <button type="submit" class="subscribe">SUBMIT</button>
+        </div>
+     
     </div>
-  );
-};
-  
-export default SignUp;
+    </div>
+    </div>
+    </div>
+
+    <footer>
+    <div className="row p-5 d-flex justify-content-center">
+      <div className="col-lg-2">
+
+     
+        <p>Need a home ? <tr></tr>Leave the hustle to us</p>
+        <img src="https://ld-wp73.template-help.com/woocommerce/prod_24608/v2/wp-content/uploads/2019/06/fb.svg" alt=""></img>
+        <img src="https://ld-wp73.template-help.com/woocommerce/prod_24608/v2/wp-content/uploads/2019/06/instagram.svg"
+          alt=""></img>
+        <img src="https://ld-wp73.template-help.com/woocommerce/prod_24608/v2/wp-content/uploads/2019/06/youtube.svg"
+          alt=""></img>
+        <img src="https://ld-wp73.template-help.com/woocommerce/prod_24608/v2/wp-content/uploads/2019/06/twitter.svg"
+          alt=""></img>
+      </div>
+
+
+
+      
+
+      <div className="col-lg-2">
+        <p className="quicklinks">Quick Links</p>
+        <p>– About</p>
+        <p>– photos</p>
+        <p>– News</p>
+        <p>– Contacts</p>
+
+      </div>
+
+      <div className="col-lg-2">
+        <p className="quicklinks">Help</p>
+        <p>– FAQs</p>
+        <p>– Updates</p>
+        <p>– Complains</p>
+        <p>– Contacts</p>
+
+      </div>
+
+      <div className="col-lg-2">
+        <p className="quicklinks">Categories</p><tr></tr>
+        <p>– FAQs</p>
+        <p>– Rentals</p>
+        <p>– Apartments</p>
+        <p>– Contacts</p>
+
+      </div>
+
+      <div className="col-lg-2">
+        <p className="quicklinks">Contacts</p>
+        <p className="number">
+          <ion-icon name="call"></ion-icon>254-100-161
+        </p>
+        <p>
+          <ion-icon name="pin"></ion-icon>254 Souths Suited 21,<tr></tr>
+           NRB
+        </p>
+        <p className="number2">
+          <ion-icon name="mail"></ion-icon>Eaglehouse@info.co.ke
+        </p>
+
+
+      </div>
+       <h6>2022 @ Eaglehousehunter.com </h6>
+    </div>
+    </footer>
+    </div>
+  )
+}
+
+
+export default SignUp
